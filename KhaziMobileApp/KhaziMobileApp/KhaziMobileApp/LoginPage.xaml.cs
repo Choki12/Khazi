d89ff87a1168891324c8a5af07b85f0fd49bf968
Boitshoko Tumane <tumanechoki@gmail.com>
2020-10-10 15:52:36 +0200

    Added Dashboard page (Tabbed), added validation for login as well as navigation.

--- KhaziMobileApp/KhaziMobileApp/KhaziMobileApp/LoginPage.xaml.cs
+++ KhaziMobileApp/KhaziMobileApp/KhaziMobileApp/LoginPage.xaml.cs
@@ -15,11 +15,33 @@ namespace KhaziMobileApp
         public LoginPage()
         {
             InitializeComponent();
+
+            SetLoginDetails("", "");
         }
 
         void OnSignInClicked(object sender, EventArgs args)
         {
+            var name = UName.Text;
+            var password = UPassword.Text;
 
+            if(name == null|| password == null)
+            {
+               DisplayAlert("Alert", "Please enter a username and password", "OK");
+            }
+            else
+            {
+                DisplayAlert("Alert", "Login Successful", "OK");
+                Navigation.PushAsync(new Dashboard());
+                
+
+            }
+            
+        }
+
+        void SetLoginDetails(string Name, string Password)
+        {
+            UName.Text = Name;
+            UPassword.Text = Password;
         }
 
         void OnRegisterClicked(object sender, EventArgs args)
