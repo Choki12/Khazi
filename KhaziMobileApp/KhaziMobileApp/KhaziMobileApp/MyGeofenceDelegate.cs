5cd14911e33dba57c0363d725e8839ad068b7223
Boitshoko Tumane <tumanechoki@gmail.com>
2020-10-11 23:54:03 +0200

    Added Geofencing services and interfaces. Still need to actually display device tracking, currently will only track phone.

---
+++ KhaziMobileApp/KhaziMobileApp/KhaziMobileApp/MyGeofenceDelegate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Shiny.Locations;
+using Shiny.Notifications;
+
+namespace KhaziMobileApp
+{
+    class MyGeofenceDelegate : IGeofenceDelegate
+    {
+        readonly INotificationManager notifications;
+        public MyGeofenceDelegate(INotificationManager notifications)
+        {
+            this.notifications = notifications;
+        }
+
+        //we make this asynchronous to wait for notifications to come in 
+        public async Task OnStatusChanged(GeofenceState newStatus, GeofenceRegion region) 
+        {
+            var msg = newStatus == GeofenceState.Entered ? "Welcome" : "Goodbye";
+            await notifications.Send("Geofence", msg);
+        }
+    }
+}
--- KhaziMobileApp/KhaziMobileApp/KhaziMobileApp/MyStartup.cs
+++ KhaziMobileApp/KhaziMobileApp/KhaziMobileApp/MyStartup.cs
@@ -1,19 +1,22 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Text;
-using Shiny;
-
+using Shiny.Notifications;
+using Shiny.Locations;
 
 namespace KhaziMobileApp
 {
 
     public class MyStartup : Shiny.ShinyStartup
-    {
+    {   
         public override void ConfigureServices(IServiceCollection services)
         {
             // register your shiny services here
-            services.UseNotifications(); // set true
+            services.UseNotifications(true); // set true
+            services.UseGeofencing<MyGeofenceDelegate>(new GeofenceRegion(
+                  "cntower",
+                   new Position(43.6425662, -79.3892455),
+                Distance.FromMeters(200)
+               ));
         }
     
     }
